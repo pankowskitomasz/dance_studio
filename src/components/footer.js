551d0de6cb87aba87d2635cf33397b7d20f6e80b
pankowskitomasz <pankowski_tomasz@o2.pl>
2021-08-01 16:25:06 +0200

    frontend: contact, login, register

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,7 @@ class Footer extends Component{
     render(){
         return(      
             <footer>
-                <Container fluid className="d-flex align-items-center p-3 fixed-bottom z-index-0">
+                <Container fluid className="bg-light-gray border-top border-dark-gray d-flex align-items-center p-3 z-index-0">
                     <Row className="mx-auto text-center d-flex w-100">
                         <Col xs={10} className="mx-auto">
                             <small className="my-0 text-secondary">
